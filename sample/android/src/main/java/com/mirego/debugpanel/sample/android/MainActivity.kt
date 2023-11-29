d1184892e0fe2362d6872b13fb8e13741159261d
Mathieu Larue <mlarue@mirego.com>
2023-11-29 17:07:44 -0500

    Cleanup

--- sample/android/src/main/java/com/mirego/debugpanel/sample/android/MainActivity.kt
+++ sample/android/src/main/java/com/mirego/debugpanel/sample/android/MainActivity.kt
@@ -3,7 +3,6 @@ package com.mirego.debugpanel.sample.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,9 +26,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     RootView(applicationViewModel.rootViewModel)
-                }
-
-                Column {
                 }
             }
         }
