8bdccdf64aefd05db68bd21c111b74f20ec755e1
Mathieu Larue <mlarue@mirego.com>
2023-12-01 08:40:23 -0500

    WIP

--- sample/ios/ios/ContentView.swift
+++ sample/ios/ios/ContentView.swift
@@ -14,33 +14,13 @@ struct ContentView: View {
     }
 
 	var body: some View {
-        VStack {
-            Text(viewModel.title)
-
-            LazyVStack(spacing: 0) {
-                VMDForEach(viewModel.debugPanel.items) { item in
-                    if let label = item as? DebugPanelItemViewModelLabel {
-                        HStack {
-                            VMDText(label.label)
-                            VMDText(label.viewModel)
-                        }
-                    } else if let toggle = item as? DebugPanelItemViewModelToggle {
-                        VMDToggle(toggle.viewModel)
-                    } else if let textField = item as? DebugPanelItemViewModelTextField {
-                        VMDTextField(textField.viewModel) {}
-                    } else if let button = item as? DebugPanelItemViewModelButton {
-                        VMDButton(button.viewModel) {
-                            Text($0.text)
-                        }
-                    } else if let picker = item as? DebugPanelItemViewModelPicker {
-                        VMDPicker(picker.viewModel, label: picker.label) {
-                            Text($0.text)
-                        }
-                    } else if let datePicker = item as? DebugPanelItemViewModelDatePicker {
-                        DatePickerView(datePicker: datePicker)
-                    }
-                }
+        ZStack {
+            VStack(alignment: .leading) {
+                Text(viewModel.title)
+                DebugPanelView(viewModel.debugPanel)
             }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding(16)
 	}
 }
