92d1e7f38ec90228bffce4fe06bda6796039a055
Vincent Ribou <VRibou@mirego.com>
2025-04-01 07:46:07 -0400

    Remove the color change

--- core/src/androidMain/kotlin/com/mirego/debugpanel/DebugPanelView.kt
+++ core/src/androidMain/kotlin/com/mirego/debugpanel/DebugPanelView.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mirego.compose.utils.SpacerHorizontal
@@ -50,8 +49,7 @@ fun DebugPanelView(
 ) {
     VMDLazyColumn(
         viewModel = viewModel.items,
-        modifier = modifier.fillMaxWidth()
-            .background(Color.Red),
+        modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) { item ->
         when (item) {
